15fb9d5cd5f51aaaf3633bc3678415a73f0cbc66
daveripper0020 <daveripper0020@gmail.com>
2024-04-01 07:45:16 +0900

    Replace mod thumbnail

--- src/components/ModEntry.svelte
+++ src/components/ModEntry.svelte
@@ -8,7 +8,8 @@
 <a href="/{id}">
     <div class="flex bg-blend-darken rounded-xl w-full h-48 p-6 transition-all duration-300
         hover:shadow-2xl hover:shadow-gray-200 hover:-translate-y-3"
-        style="background: linear-gradient(to bottom, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.85)), url({image}) no-repeat center;">
+        style="background: linear-gradient(to bottom, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.85)), url({image})
+        no-repeat center; background-size: cover;">
         <div class="flex flex-col self-end text-white">
             <span class="font-bold">{title}</span>
             <span>{description}</span>
