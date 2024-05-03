4fe0c4fac2ad09caa87a2d2a0979c65b7a9071f2
daveripper0020 <daveripper0020@gmail.com>
2024-05-03 16:47:14 +0900

    Add LargeImageViewer component

---
+++ src/components/LargeImageViewer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { expoOut } from "svelte/easing";
+    import { fade } from "svelte/transition";
+
+    export let src: string | undefined = undefined;
+
+    function close()
+    {
+        src = undefined;
+    }
+</script>
+
+{#if src}
+    <div transition:fade={{ easing: expoOut, duration: 250 }} role="button" tabindex="0" on:click={close} on:keydown={close}>
+        <img class="fixed max-w-[calc(100vw-10%)] max-h-[calc(100vh-10%)] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 rounded-xl"
+            {src} alt="Focused" />
+        <div class="fixed top-0 left-0 w-screen h-screen bg-black/50 z-10"></div>
+    </div>
+{/if}
