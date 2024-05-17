ece73824672ae30e99ccc49f7ab68ff8721373a4
daveripper0020 <daveripper0020@gmail.com>
2024-05-18 03:28:43 +0900

    Change to adapter-auto

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,6 @@
+import { locale, waitLocale } from "svelte-i18n";
 import { browser } from "$app/environment";
 import "$lib/i18n"; //import to init
-import { locale, waitLocale } from "svelte-i18n";
-
-export const prerender = true;
 
 export async function load()
 {
