7e50b90ade81775c88db901be82e566012f3d6cc
daveripper0020 <daveripper0020@gmail.com>
2024-06-02 04:13:40 +0900

    Improve behaviour of LargeImageViewer on small images

--- src/components/LargeImageViewer.svelte
+++ src/components/LargeImageViewer.svelte
@@ -12,7 +12,8 @@
 
 {#if src}
     <div transition:fade={{ easing: expoOut, duration: 250 }} role="button" tabindex="0" on:click={close} on:keydown={close}>
-        <img class="fixed max-w-[calc(100vw-10%)] max-h-[calc(100vh-10%)] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 rounded-xl"
+        <img class="fixed min-h-[60%] max-w-[calc(100vw-10%)] max-h-[calc(100vh-10%)]
+            top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 rounded-xl"
             {src} alt="Focused" />
         <div class="fixed top-0 left-0 w-screen h-screen bg-black/50 z-10"></div>
     </div>
